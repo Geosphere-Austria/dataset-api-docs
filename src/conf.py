6da79ab07bb1639c69846d98460cb17f6800a35f
Fabian Pechstein <fabian.pechstein@geosphere.at>
2025-06-04 17:33:41 +0200

    github_version is "main"

--- src/conf.py
+++ src/conf.py
@@ -69,7 +69,7 @@ html_context = {
     "display_github": True, # Integrate GitHub
     "github_user": "Geosphere-Austria", # Username
     "github_repo": "dataset-api-docs", # Repo name
-    "github_version": "master", # Version
+    "github_version": "main", # Version
     "conf_py_path": "/src/", # Path in the checkout to the docs root
 }
 
